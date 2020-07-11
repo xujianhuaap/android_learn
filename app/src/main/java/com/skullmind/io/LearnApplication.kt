68940d1d273281f1e78d30acfdad433246fac405
skullmind <xujianhuaap@gmail.com>
2020-07-11 10:38:22 +0800

    featuer[LearnApplication]:init branch to develop

--- app/src/main/java/com/skullmind/io/LearnApplication.kt
+++ app/src/main/java/com/skullmind/io/LearnApplication.kt
@@ -1,26 +1,13 @@
 package com.skullmind.io
 
-import android.app.Activity
 import android.app.Application
-import android.content.Context
-import com.skullmind.io.dagger.component.DaggerAppComponent
-import dagger.android.*
-import javax.inject.Inject
 
-class LearnApplication:Application(),HasActivityInjector{
-    @Inject
-    lateinit var mActivityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>;
-    @Inject
-    lateinit var mContext:Context;
+class LearnApplication:Application(){
     override fun onCreate() {
         super.onCreate()
-        DaggerAppComponent.builder()
-            .application(this)
-            .build()
-            .inject(this)
+
 
 
     }
 
-    override fun activityInjector(): AndroidInjector<Activity> = mActivityDispatchingAndroidInjector;
 }
