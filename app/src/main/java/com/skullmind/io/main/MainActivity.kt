1500a681661afb6bd89e1ebe009aba88bb3ea9db
skullmind <xujianhuaap@gmail.com>
2020-07-11 10:41:20 +0800

    featuer[activity_main.xml]:init branch to develop

--- app/src/main/java/com/skullmind/io/main/MainActivity.kt
+++ app/src/main/java/com/skullmind/io/main/MainActivity.kt
@@ -2,11 +2,13 @@ package com.skullmind.io.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.skullmind.io.R
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
     }
 
