e28cfa1b50ba94d788626b68e83dc3470cf54d47
skullmind <xujianhuaap@gmail.com>
2020-07-11 10:36:27 +0800

    featuer[MainActivity AndroidManifenst.xml]:init branch to develop

--- app/src/main/java/com/skullmind/io/main/MainActivity.kt
+++ app/src/main/java/com/skullmind/io/main/MainActivity.kt
@@ -1,58 +1,14 @@
 package com.skullmind.io.main
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.skullmind.io.camera.newIntentToCamera
-import com.skullmind.io.databinding.ActivityMainBinding
-import com.skullmind.io.github.newIntentToGitHub
-import dagger.android.AndroidInjection
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(){
 
-
-    @Inject
-    lateinit var binding: ActivityMainBinding
-    @Inject
-    lateinit var vm:MainVM
-    @Inject
-    lateinit var model: MainModel
-
-    @Inject
-    lateinit var cameraModel: CameraModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        initData()
-        binding.tvJump.setOnClickListener {
-            jumpToGitHub(model.userName)
-        }
-        binding.llCamera.btnCamera.setOnClickListener{
-            jumpToCamera("Cannon")
-        }
 
     }
 
-    private fun initData() {
-        binding.vm = this.vm
-        binding.model = this.model
-        binding.cameraModel = this.cameraModel
-        binding.vm!!.initData()
-    }
-
-
-
-    fun showToast(message:String){
-        Toast.makeText(this,message, Toast.LENGTH_LONG).show();
-    }
-
-    fun jumpToGitHub(userName:String){
-        startActivity(newIntentToGitHub(this,userName))
-    }
-    fun jumpToCamera(cameraName:String){
-        startActivity(newIntentToCamera(this,cameraName))
-    }
 
 }
