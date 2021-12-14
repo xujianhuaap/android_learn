44b4370616bfcbc30590b8b72e820e121df8a9b7
skullmind <xujianhuaap@gmail.com>
2021-12-14 11:15:00 +0800

    anim add and height measure

--- app/src/main/java/com/skullmind/io/main/MainActivity.kt
+++ app/src/main/java/com/skullmind/io/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.skullmind.io.main
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +12,7 @@ import com.skullmind.io.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(),MainNavigation {
+class MainActivity : AppCompatActivity(), MainNavigation {
 
 
     @Inject
@@ -38,15 +39,19 @@ class MainActivity : AppCompatActivity(),MainNavigation {
     }
 
     override fun showActivityMessage() {
-        Toast.makeText(this,MainActivity::class.java.name,Toast.LENGTH_LONG).show()
+        Toast.makeText(this, MainActivity::class.java.name, Toast.LENGTH_LONG).show()
     }
-    override fun showPopWindow() {
 
-       PopupWindow(this).apply {
-            contentView = View.inflate(this@MainActivity, R.layout.window_pop_up,null)
-            isOutsideTouchable= true
-           val offsetY = binding.tvContent3.measuredHeight + resources.getDimensionPixelOffset(R.dimen.dimen_50_dp)
-           showAsDropDown(binding.tvContent3,0,-offsetY,Gravity.BOTTOM)
+    override fun showPopWindow() {
+        val contentView = View.inflate(this@MainActivity, R.layout.window_pop_up, null).apply {
+            measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        }
+        PopupWindow(contentView,ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT).apply {
+            this.animationStyle = R.style.BasePopWindowAnim
+            this.elevation = 0f
+            this.isOutsideTouchable = true
+            val offsetY = binding.tvContent3.measuredHeight+contentView.measuredHeight
+            this.showAsDropDown(binding.tvContent3, 0, -offsetY, Gravity.CENTER)
         }
 
 
---
+++ app/src/main/java/com/skullmind/io/widget/BasePopupWindow.kt
@@ -0,0 +1,8 @@
+package com.skullmind.io.widget
+
+import android.content.Context
+import android.widget.PopupWindow
+
+class BasePopupWindow(val context: Context) :PopupWindow(context) {
+
+}
