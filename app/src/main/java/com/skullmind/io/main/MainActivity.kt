66ab66a49e2da306211d692308bb106dbc5320e9
xujianhua <1091041307@qq.com>
2022-03-17 21:05:01 +0800

    feature:compose layout basic use

--- app/src/main/java/com/skullmind/io/main/MainActivity.kt
+++ app/src/main/java/com/skullmind/io/main/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.skullmind.io.main
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.skullmind.io.R
 
 
 class MainActivity : AppCompatActivity(){
@@ -22,9 +29,31 @@ class MainActivity : AppCompatActivity(){
         Text(text = "Hello $name!")
     }
 
+    @Composable
+    fun LoginCard(){
+        Row() {
+            Box(){
+                Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "personal image")
+                Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "personal image")
+
+            }
+
+            Column() {
+                Text(text = "LoginName")
+                Text(text = "Password")
+            }
+        }
+
+
+    }
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard("Android")
+        Column() {
+            LoginCard()
+        }
+
     }
 }
