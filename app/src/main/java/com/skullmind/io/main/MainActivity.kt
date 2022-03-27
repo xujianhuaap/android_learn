b3c726c1be758530430c4ad9c996761728e52a5a
= <=>
2022-03-27 21:45:53 +0800

    feature:RxJava filter

--- app/src/main/java/com/skullmind/io/main/MainActivity.kt
+++ app/src/main/java/com/skullmind/io/main/MainActivity.kt
@@ -3,12 +3,18 @@ package com.skullmind.io.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.skullmind.io.R
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Observable.create(ObservableOnSubscribe<String> {
+
+        }).subscribeOn(Schedulers.io()).subscribe()
 
     }
 
