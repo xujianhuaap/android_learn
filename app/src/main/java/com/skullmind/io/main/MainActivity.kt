ab0bd0b524fa50cbf170406e13762f7819515aa3
= <=>
2022-03-30 21:34:58 +0800

    feature:zip use

--- app/src/main/java/com/skullmind/io/main/MainActivity.kt
+++ app/src/main/java/com/skullmind/io/main/MainActivity.kt
@@ -3,11 +3,11 @@ package com.skullmind.io.main
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.skullmind.io.Net
 import com.skullmind.io.R
 import com.skullmind.io.User
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
@@ -25,17 +25,16 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val info = Net.createService(User::class.java).getInfo("defunkt").subscribeOn(Schedulers.io())
         val repos = Net.createService(User::class.java).getRepos("defunkt").subscribeOn(Schedulers.io())
-        fullInfo(info,repos).subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread()).subscribe(MyObserver())
+        fullInfo(info,repos).subscribe(MyObserver())
     }
 
-    private fun fullInfo(info: Observable<JsonObject>,repo: Observable<JsonObject>) =
+    private fun fullInfo(info: Observable<JsonObject>,repo: Observable<JsonArray>) =
         Observable.zip(info,repo,this::zipOperator,true)
 
 
-    private fun zipOperator(info:JsonObject,repo:JsonObject):JsonObject = JsonObject().apply {
+    private fun zipOperator(info:JsonObject, repos:JsonArray):JsonObject = JsonObject().apply {
          this.add("info",info)
-         this.add("repo",repo)
+         this.add("repos",repos)
     }
 
 }
