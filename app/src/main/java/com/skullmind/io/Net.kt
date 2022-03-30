ed5218d366ff9debf8b8d83caeb0871a3dd491eb
xujianhua <1091041307@qq.com>
2022-03-30 16:45:59 +0800

    feature:retrofit rxjava upgrade version

---
+++ app/src/main/java/com/skullmind/io/Net.kt
@@ -0,0 +1,24 @@
+package com.skullmind.io
+
+import com.google.gson.JsonObject
+import io.reactivex.rxjava3.core.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+object Net {
+    private fun init() =  Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
+
+    fun <T>createService(clazz: Class<T>) = init().create(clazz)
+}
+
+interface User {
+    @GET("users/{username}")
+    fun getInfo(@Path("username") username:String):Observable<JsonObject>
+}
--- app/src/main/java/com/skullmind/io/main/MainActivity.kt
+++ app/src/main/java/com/skullmind/io/main/MainActivity.kt
@@ -1,22 +1,43 @@
 package com.skullmind.io.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.JsonObject
+import com.skullmind.io.Net
 import com.skullmind.io.R
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import com.skullmind.io.User
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Observable.create(ObservableOnSubscribe<String> {
-
-        }).subscribeOn(Schedulers.io()).subscribe()
-
+        Net.createService(User::class.java).getInfo("defunkt").observeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread()).subscribe(MyObserver())
     }
 
 
+}
+
+class MyObserver : Observer<JsonObject> {
+    override fun onSubscribe(d: Disposable) {
+
+    }
+
+    override fun onNext(t: JsonObject) {
+        Log.d("-->", "MyObserver:success ->" + t)
+    }
+
+    override fun onError(e: Throwable) {
+
+    }
+
+    override fun onComplete() {
+        Log.d("-->", "MyObserver:onComplete ->" + Thread.currentThread().name)
+    }
 }
