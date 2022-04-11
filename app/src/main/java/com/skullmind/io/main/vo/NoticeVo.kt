feae1c019347c9c281b923704270b7fa0dc32511
= <=>
2022-04-11 09:25:44 +0800

    feature:MarqueeView fix

--- app/src/main/java/com/skullmind/io/main/vo/NoticeVo.kt
+++ app/src/main/java/com/skullmind/io/main/vo/NoticeVo.kt
@@ -9,7 +9,7 @@ import kotlinx.parcelize.Parcelize
 }
 
 fun getNotices() = listOf<NoticeVo>(
-    NoticeVo("南航新会员活动","https://"),
-    NoticeVo("快乐飞无限","https://"),
+    NoticeVo("南航新会员活动,欢迎登陆官网领取","https://"),
+    NoticeVo("快乐飞无限激情四月，最super的你一起来","https://"),
     NoticeVo("加油广州","https://")
 )
--- app/src/main/java/com/skullmind/io/main/widget/MarqueeView.kt
+++ app/src/main/java/com/skullmind/io/main/widget/MarqueeView.kt
@@ -2,58 +2,67 @@ package com.skullmind.io.main.widget
 
 import android.util.Log
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.skullmind.io.main.vo.NoticeVo
+import kotlinx.coroutines.delay
 
 @Composable
 fun MarqueeView(datas: List<NoticeVo>, initIndex: Int) {
-    val infiniteTransition = rememberInfiniteTransition()
-    val alpha by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 1.1f,
-        animationSpec = infiniteRepeatable(
-            animation = keyframes {
-                durationMillis = 4000
-                delayMillis = 100
-            },
-            repeatMode = RepeatMode.Restart
-        )
-    )
 
 
-    MarqueeContainer(alpha, datas, initIndex = initIndex)
+    MarqueeContainer(datas, initIndex = initIndex)
 
 }
 
 @Composable
-private fun MarqueeContainer(alpha: Float, datas: List<NoticeVo>, initIndex: Int) {
+private fun MarqueeContainer(datas: List<NoticeVo>, initIndex: Int) {
 
     var index by remember {
         mutableStateOf(initIndex)
     }
-    if (alpha >= 1f) {
-        index = (index + 1) % datas.size
 
+    var paddingOffset by remember {
+        mutableStateOf(0f)
+    }
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(15)
+            paddingOffset += 0.6f
+        }
     }
 
     val clickState = remember {
-        mutableStateOf(Pair<Boolean, NoticeVo>(false, datas[0]))
+        mutableStateOf(Pair(false, datas[0]))
     }
 
-    Row(modifier = Modifier.padding(start = (LocalConfiguration.current.screenWidthDp * alpha).dp)) {
+    Row(
+        modifier = Modifier
+            .padding(
+                start = paddingOffset.dp,
+            )
+    ) {
         Text(
             text = datas[index].title,
             maxLines = 1,
-            modifier = Modifier.clickable {
-                clickState.value = Pair(true,datas[index])
-            }.padding(vertical = 5.dp)
+            modifier = Modifier.padding(vertical = 5.dp),
+            overflow = TextOverflow.Clip,
+            onTextLayout = {
+                if (it.size.width == 0) {
+                    index = (index + 1) % datas.size
+                    paddingOffset = 0f
+                }
+                Log.d("-->", "text width ${it.size.width}")
+            }
+
         )
     }
 
